0590f8625bed33afe7aacecf191d253d5d288f18
Will Farley <wfarley@amazon.com>
2019-11-29 17:18:11 -0800

    Update node-sass to v4.13

--- test/unit/render.test.js
+++ test/unit/render.test.js
@@ -4,7 +4,7 @@ var render = require('../../lib/render'),
 
 describe('lib/render', function() {
     it('should work as expected', function() {
-        return render('fixtures/render-1.scss', {outputStyle: "compressed"})
+        return render('test/fixtures/render-1.scss', {outputStyle: "compressed"})
         .then(function(css) {
             expect(css).to.equal("body{color:red}\n");
         });
@@ -15,7 +15,7 @@ describe('lib/render', function() {
 
         var tmp = temp.openSync();
 
-        return render('fixtures/render-1.scss', tmp.path, {outputStyle: "compressed"})
+        return render('test/fixtures/render-1.scss', tmp.path, {outputStyle: "compressed"})
         .then(function() {
             var content = fs.readFileSync(tmp.path);
             expect(content.toString()).to.equal("body{color:red}\n");
@@ -23,7 +23,7 @@ describe('lib/render', function() {
     });
 
     it("should resolve output base on `root` file if it's relative", function() {
-        return render('fixtures/render-1.scss', 'fixtures/output.css', {outputStyle: "compressed"})
+        return render('test/fixtures/render-1.scss', 'test/fixtures/output.css', {outputStyle: "compressed"})
         .then(function(out) {
             var content = fs.readFileSync(out);
             expect(content.toString()).to.equal("body{color:red}\n");
